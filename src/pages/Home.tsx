c01c8025db56749399bf01058a8f63f607d20492
serenantonini <s.antonini7@campus.unimib.it>
2025-10-23 22:16:45 +0200

    fix immagine home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,19 @@
 import Layout from "@/components/Layout";
-import sfondo from "@/assets/sfondo4.jpeg"; // importa l'immagine
+import sfondo from "@/assets/sfondo4.jpeg";
 
 const Home = () => {
   return (
     <Layout className="pt-0">
       <div
-        className="h-screen w-full relative bg-cover bg-center flex items-center justify-center"
-        style={{ backgroundImage: `url(${sfondo})` }} // usa l'immagine come sfondo
+        className="w-full flex-grow flex items-center justify-center bg-cover bg-center min-h-[calc(100vh-73px-56px)]"
+        style={{
+          backgroundImage: `url(${sfondo})`,
+        }}
       >
-        
+        {/* eventuale contenuto sopra l'immagine */}
       </div>
     </Layout>
   );
 };
 
 export default Home;
-
